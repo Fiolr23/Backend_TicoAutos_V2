a16ac100a795a1942726f920decde1cd6409e730
Fiorella <fiorellalazo25@gmail.com>
2026-04-06 14:26:31 -0600

    se agregó el endpoint para la validacion de cedula

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { register } = require("../controllers/userController");
+const { register, validateCedula } = require("../controllers/userController");
+
+// Este endpoint lo usa el formulario para consultar MI backend
+// y obtener nombre + apellidos desde el padron.
+router.get("/validate-cedula", validateCedula);
 
 router.post("/register", register);
 
