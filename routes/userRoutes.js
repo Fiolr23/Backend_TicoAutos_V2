f024923fa44b3845d3645c64cddb598986f1de7c
Nayely Vega <nayevm04@gmail.com>
2026-04-06 23:46:24 -0600

    Actualiza logica de usuario para cedula y Google

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { register, validateCedula } = require("../controllers/userController");
+const { register, registerWithGoogle, validateCedula } = require("../controllers/userController");
 
 // Este endpoint lo usa el formulario para consultar MI backend
 // y obtener nombre + apellidos desde el padron.
 router.get("/validate-cedula", validateCedula);
 
 router.post("/register", register);
+router.post("/register-google", registerWithGoogle);
 
 module.exports = router;
