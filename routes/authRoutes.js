cf61b5ace97108db9bd26645cc07953d888916a0
Fiorella <fiorellalazo25@gmail.com>
2026-04-09 16:41:32 -0600

    Agregación de ruta de verificación de correo

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { generateToken, googleAuth, authenticateToken, logout } = require("../middleware/auth");
+const { generateToken, googleAuth, authenticateToken, logout, verifyEmail } = require("../middleware/auth");
 
 // POST /api/auth/login
 router.post("/login", generateToken);
 
 // POST /api/auth/google
 router.post("/google", googleAuth);
+
+// Ruta que verifica el correo al abrir el link.
+router.get("/verify-email", verifyEmail);
 
 // POST /api/auth/logout (protegida)
 router.post("/logout", authenticateToken, logout);
@@ -16,7 +19,7 @@ router.post("/logout", authenticateToken, logout);
 router.get("/me", authenticateToken, (req, res) => {
   res.json({
     message: "Token válido",
-    user: { id: req.user._id, name: req.user.name, lastname: req.user.lastname, email: req.user.email },
+    user: { id: req.user._id, name: req.user.name, lastname: req.user.lastname, email: req.user.email, accountStatus: req.user.accountStatus },
   });
 });
 
